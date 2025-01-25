3c4df2e69f37c67f232106ffeb14b27466d9ddc3
Andreas Kröpelin <andreas-kroepelin@gmx.de>
2025-01-25 14:32:19 +0100

    Change publishing strategy

--- scripts/extract-package.fish
+++ scripts/extract-package.fish
@@ -1,20 +1,15 @@
-function extract-package
-    if test (git branch --show-current) != "release"
-        echo "You are not on the release branch!"
-        return 1
-    end
-
-    set target $argv[1]
-    pwd
-    mkdir -p $target
-    cp README.md $target
-    cp LICENSE $target
-    cp typst.toml $target
-    cp polylux.typ $target
-    cp logic.typ $target
-    mkdir -p $target/themes
-    cp themes/* $target/themes
-    mkdir -p $target/utils
-    cp utils/* $target/utils
-    echo "Done"
+if test (git branch --show-current) != release
+    echo "You are not on the release branch!"
+    return 1
 end
+
+set target $argv[1]
+pwd
+mkdir -p $target
+cp README.md $target
+cp LICENSE $target
+cp typst.toml $target
+cp -a src $target
+cp -a examples $target
+cp -a assets $target
+echo Done
